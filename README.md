efb5cb7d713ab490667e9709df4fed41c7a5ce4c
Matthew Westrik <m@ttwestrik.com>
2016-06-15 17:28:51 -0400

    Use keras instead of lasagne

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Machine translation with RNNs
 
-Implementation of the language model described in "Neural Machine Translation by
+Implementation of the language translation model described in "Neural Machine Translation by
 Jointly Learning to Align and Translate" by D. Bahdanau, K. Cho, and Y. Bengio.
 ([arXiv](http://arxiv.org/abs/1409.0473))
 
 Implemented in Python using [Theano](https://github.com/Theano/Theano) and
-[Lasagne](https://github.com/Lasagne/Lasagne).
+[Keras](https://github.com/fchollet/keras).
 
 <!--- ////////////////// -->
 
--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 import numpy as np
 import theano
 import theano.tensor as T
-import lasagne
+import keras
 
 
 class RNNEncDecAttnModel:
@@ -20,38 +20,6 @@ class RNNEncDecAttnModel:
 	# Recurrent layers expect input of shape
 	# (batch size, SEQ_LENGTH, num_features)
 
-	l_in = lasagne.layers.InputLayer(shape=(None, None, 30000))
-	'''
-
-	# We now build the LSTM layer which takes l_in as the input layer
-	# We clip the gradients at GRAD_CLIP to prevent the problem of exploding gradients. 
-
-	l_forward_1 = lasagne.layers.LSTMLayer(
-	    l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,
-	    nonlinearity=lasagne.nonlinearities.tanh)
-
-	l_forward_2 = lasagne.layers.LSTMLayer(
-	    l_forward_1, N_HIDDEN, grad_clipping=GRAD_CLIP,
-	    nonlinearity=lasagne.nonlinearities.tanh)
-
-	# The l_forward layer creates an output of dimension (batch_size, SEQ_LENGTH, N_HIDDEN)
-	# Since we are only interested in the final prediction, we isolate that quantity and feed it to the next layer. 
-	# The output of the sliced layer will then be of size (batch_size, N_HIDDEN)
-	l_forward_slice = lasagne.layers.SliceLayer(l_forward_2, -1, 1)
-
-	# The sliced output is then passed through the softmax nonlinearity to create probability distribution of the prediction
-	# The output of this stage is (batch_size, vocab_size)
-	l_out = lasagne.layers.DenseLayer(l_forward_slice, num_units=vocab_size, W = lasagne.init.Normal(), nonlinearity=lasagne.nonlinearities.softmax)
-
-	# Theano tensor for the targets
-	target_values = T.ivector('target_output')
-	
-	# lasagne.layers.get_output produces a variable for the output of the net
-	network_output = lasagne.layers.get_output(l_out)
-
-	# The loss function is calculated as the mean of the (categorical) cross-entropy between the prediction and target.
-	cost = T.nnet.categorical_crossentropy(network_output,target_values).mean()
-	'''
 
 
 
